63ccfc4f0f4ea6f886dd92fa074e501fc17f417f
MajaSwiderska <150272450+MajaSwiderska@users.noreply.github.com>
2026-09-12 12:54:42 -0500

    Updated Ellipse bounding box calculation to use doubled width and height for accurate dimensions.

--- src/main/scala/edu/luc/cs/laufer/cs371/shapes/boundingBox.scala
+++ src/main/scala/edu/luc/cs/laufer/cs371/shapes/boundingBox.scala
@@ -7,7 +7,7 @@ import Shape.*
 object boundingBox:
   def apply(s: Shape): Location = s match
     case Rectangle(w, h) => Location(0, 0, Rectangle(w, h))
-    case Ellipse(w, h) => Location(0, 0, Rectangle(w, h))
+    case Ellipse(w, h) => Location(0, 0, Rectangle(2 * w, 2 * h))
     case Location(x, y, shape) =>
       val Location(bx, by, Rectangle(bw, bh)) = apply(shape): @unchecked
       Location(x + bx, y + by, Rectangle(bw, bh))
