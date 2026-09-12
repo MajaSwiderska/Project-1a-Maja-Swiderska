dc8e3f896d4ba2ad1c7c6d6b28e1b82347f55165
MajaSwiderska <150272450+MajaSwiderska@users.noreply.github.com>
2026-09-12 14:57:47 -0500

    Refactor indentation in height and size objects for consistency

--- src/main/scala/edu/luc/cs/laufer/cs371/shapes/height.scala
+++ src/main/scala/edu/luc/cs/laufer/cs371/shapes/height.scala
@@ -4,15 +4,15 @@ import Shape.*
 import com.typesafe.scalalogging.Logger
 
 object height:
-    private val logger = Logger(getClass)
+  private val logger = Logger(getClass)
 
-    def apply(s: Shape): Int =
+  def apply(s: Shape): Int =
     logger.debug(s"height input: $s")
     s match
-        case Rectangle(_, _) => 1
-        case Ellipse(_, _)   => 1
-        case Location(_, _, shape) => 1 + apply(shape)
-        case Group(shapes*) =>
-            if shapes.isEmpty then 1
-            else 1 + shapes.map(apply).max
+      case Rectangle(_, _) => 1
+      case Ellipse(_, _)   => 1
+      case Location(_, _, shape) => 1 + apply(shape)
+      case Group(shapes*)  =>
+        if shapes.isEmpty then 1
+        else 1 + shapes.map(apply).max
 end height
--- src/main/scala/edu/luc/cs/laufer/cs371/shapes/size.scala
+++ src/main/scala/edu/luc/cs/laufer/cs371/shapes/size.scala
@@ -4,13 +4,13 @@ import Shape.*
 import com.typesafe.scalalogging.Logger
 
 object size:
-    private val logger = Logger(getClass)
+  private val logger = Logger(getClass)
 
-    def apply(s: Shape): Int =
+  def apply(s: Shape): Int =
     logger.debug(s"size input: $s")
     s match
-        case Rectangle(_, _) => 1
-        case Ellipse(_, _)   => 1
-        case Location(_, _, shape) => apply(shape)
-        case Group(shapes*) => shapes.map(apply).sum
+      case Rectangle(_, _) => 1
+      case Ellipse(_, _)   => 1
+      case Location(_, _, shape) => apply(shape)
+      case Group(shapes*)  => shapes.map(apply).sum
 end size
